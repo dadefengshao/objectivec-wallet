fef896cbbf541933887e348e182914c07e7f2d94
Julianny Favinha <julianny.favinha@gmail.com>
2018-07-25 09:48:15 -0300

    Entry class

---
+++ Wallet/Wallet/Model/Entry.h
@@ -0,0 +1,13 @@
+//
+//  Entry.h
+//  Wallet
+//
+//  Created by Julianny Favinha on 7/24/18.
+//  Copyright © 2018 Julianny Favinha. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Entry : NSObject
+
+@end
---
+++ Wallet/Wallet/Model/Entry.m
@@ -0,0 +1,13 @@
+//
+//  Entry.m
+//  Wallet
+//
+//  Created by Julianny Favinha on 7/24/18.
+//  Copyright © 2018 Julianny Favinha. All rights reserved.
+//
+
+#import "Entry.h"
+
+@implementation Entry
+
+@end
